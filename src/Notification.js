a034ea76ba3f1cad20f4ab665b7a7bdc8cc692da
Trevor Gerhardt <trevorgerhardt@gmail.com>
2017-03-04 10:43:55 +0100

    refactor(react-toastr): Remove react-toastr
    BREAKING CHANGE: Notifications no longer work.

--- src/Notification.js
+++ src/Notification.js
@@ -1,30 +1,11 @@
-import React, { Component } from 'react';
-
-import { ToastContainer, ToastMessage } from '@allenfang/react-toastr';
-
-
-const ToastrMessageFactory = React.createFactory(ToastMessage.animation);
+import { Component } from 'react';
 
 class Notification extends Component {
   // allow type is success,info,warning,error
-  notice(type, msg, title) {
-    this.refs.toastr[type](
-      msg, title, {
-        mode: 'single',
-        timeOut: 5000,
-        extendedTimeOut: 1000,
-        showAnimation: 'animated  bounceIn',
-        hideAnimation: 'animated bounceOut'
-      });
-  }
+  notice(type, msg, title) {}
 
   render() {
-    return (
-      <ToastContainer ref='toastr'
-        toastMessageFactory={ ToastrMessageFactory }
-        id='toast-container'
-        className='toast-top-right'/>
-    );
+    return null
   }
 }
 
